d47ee67f9c9b9405d3794ae61e76a75036eea1fb
Sr-Chandan-Sahu <cs154896@gmail.com>
2026-09-09 02:05:44 +0530

    typo fix

--- src/components/ResumeSection.tsx
+++ src/components/ResumeSection.tsx
@@ -198,7 +198,7 @@ export default function ResumeSection() {
               <polyline points="7 10 12 15 17 10" />
               <line x1="12" y1="15" x2="12" y2="3" />
             </svg>
-            <span>Download Resume / CV</span>
+            <span>Download Resume</span>
           </a>
         </div>
 
